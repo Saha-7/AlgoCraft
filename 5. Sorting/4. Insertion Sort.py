690174ef202ac3e68ff905eac1911dd2e0740c54
saha-7op <saha7pritam@gmail.com>
2025-07-29 22:19:43 +0530

    sorting

--- 5. Sorting/4. Insertion Sort.py
+++ 5. Sorting/4. Insertion Sort.py
@@ -1,13 +1,13 @@
 l=[10,5,8,2,-4,-2,15]
 
 def insertion_sort(arr):
-    n=len(arr)
-    for i in range(1,n):
-        for j in range(i,0,-1):
-            if arr[j-1]>arr[j]:
-                arr[j-1],arr[j]=arr[j],arr[j-1]
-            else:
-                break
+    for i in range(len(arr)):
+        j=i-1
+        while j>=0 and arr[j+1]<arr[j]:
+            temp = arr[j+1]
+            arr[j+1] =arr[j]
+            arr[j] =temp
+            j-=1
 
 insertion_sort(l)
 print(l)
