37b7c0e4dee243fc74bff060358a311e610a532e
saha-7op <saha7pritam@gmail.com>
2025-08-16 08:41:44 +0530

    Duplicate

---
+++ NeetCode/NeetCode 250/Arrays & Hashing/2. Contains Duplicate.py
@@ -0,0 +1,20 @@
+class Solution(object):
+    def containsDuplicate(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: bool
+        """
+        
+        # for i in range(len(nums)):
+        #     for j in range(i+1,len(nums)):
+        #         if nums[i]==nums[j]:
+        #             return True
+        # return False
+
+        uniq = set()
+        for ele in nums:
+            if ele in uniq:
+                return True
+            else:
+                uniq.add(ele)
+        return False
