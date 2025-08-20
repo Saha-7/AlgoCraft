a4bb2c8b0d59835995bd443d7af79899eb3b716e
saha-7op <saha7pritam@gmail.com>
2025-08-20 09:26:02 +0530

    Search insert pos.

---
+++ NeetCode/NeetCode 250/Binary Search/2. Search Insert Position.py
@@ -0,0 +1,28 @@
+class Solution:
+    def searchInsert(self, nums: list[int], target: int) -> int:
+        left=0
+        right=len(nums)-1
+
+        while left<=right:
+            mid=(left+right)//2
+
+            if nums[mid]==target:
+                return mid
+            elif nums[mid]<target:
+                left=mid+1
+            else:
+                right=mid-1
+
+        if nums[mid]<target:
+            return mid+1
+        else:
+            return mid
+        
+
+nums = [-1,0,2,4,6,8]
+target = 5
+
+sol= Solution()
+print(sol.searchInsert(nums,target))
+
+
