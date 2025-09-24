5326da7e1dbca344f8da57a49568558c0e041c82
saha-7op <saha7pritam@gmail.com>
2025-09-24 21:13:45 +0530

    valid anagram

---
+++ NeetCode/NeetCode 250/Arrays & Hashing/3. Valid Anagram.py
@@ -0,0 +1,36 @@
+class Solution(object):
+    def isAnagram(self, s, t):
+        """
+        :type s: str
+        :type t: str
+        :rtype: bool
+        """
+        # sol 1
+        if len(s) != len(t):
+            return False
+
+        countS, countT = {}, {}
+
+        for i in range(len(s)):
+            countS[s[i]] = 1 + countS.get(s[i], 0)
+            countT[t[i]] = 1 + countT.get(t[i], 0)
+
+        return countS == countT
+    
+
+
+
+
+        # sol 2
+        
+# from collections import Counter
+
+# class Solution(object):
+#     def isAnagram(self, s, t):
+#         if len(s) != len(t):
+#             return False
+        
+#         dict_s=Counter(s)
+#         dict_t=Counter(t)
+
+#         return dict_s==dict_t
